bc680b512c59c013e25d1f1a7e94ec899b735ef8
Sergey Xursevich <hursik94@gmail.com>
2019-05-17 23:14:30 +0300

    add: search word length

--- src/js/App.js
+++ src/js/App.js
@@ -1,19 +1,53 @@
 import Data from './Data';
 
 export default class App {
-    constructor() {
-        this.state = {
-            url: 'http://www.mrsoft.by/data.json',
-            proxyurl: 'https://cors-anywhere.herokuapp.com/',
-        }
-    }
+  constructor() {
+    this.state = {
+      url: 'http://www.mrsoft.by/data.json',
+      proxyurl: 'https://cors-anywhere.herokuapp.com/',
+    };
+  }
 
-    async start() {
-        const data = new Data(this.state);
-        const arr = await data.getData();
-        arr.forEach(element => {
-            console.log(element + "///");
-            
-        });
-    }
+  async start() {
+    const searchLength = document.querySelector('#btn-search-length');
+    const searchSubstring = document.querySelector('#btn-search-substring');
+    // const checkbox = document.querySelector('#checkbox');
+    const inputText = document.querySelector('#inputText');
+    const textarea = document.querySelector('.textarea');
+
+    const data = new Data(this.state);
+    const arr = await data.getData();
+
+    searchLength.addEventListener('click', () => {
+      const countWord = document.getElementsByTagName('p');
+
+      if (countWord.length) {
+        textarea.innerHTML = ' ';
+      }
+
+      const { value } = inputText;
+
+      arr.forEach((element) => {
+        if (value.length < element.length) {
+          textarea.innerHTML += `<p>${element}</p>`;
+        }
+      });
+    });
+
+    inputText.addEventListener('keyup', () => {
+      if (String(inputText.value).length > 1) {
+        searchLength.classList.remove('btn__disable');
+        searchLength.removeAttribute('disabled');
+
+        searchSubstring.classList.remove('btn__disable');
+        searchSubstring.removeAttribute('disabled');
+      } else {
+        searchLength.classList.add('btn__disable');
+        searchSubstring.classList.add('btn__disable');
+
+        searchLength.setAttribute('disabled', 'disabled');
+        searchSubstring.setAttribute('disabled', 'disabled');
+      }
+    });
+  }
 }
--- src/js/Data.js
+++ src/js/Data.js
@@ -1,19 +1,19 @@
 export default class Data {
-    constructor(state) {
-        this.state = state;
-        this.result = [];
-    }
+  constructor(state) {
+    this.state = state;
+    this.result = [];
+  }
 
-    static extractData(data) {
-        return data.data.map(value => value);
-    }
+  static extractData(data) {
+    return data.data.map(value => value);
+  }
 
-    async getData() {
-        const { proxyurl, url } = this.state;
-       
-        const responce = await fetch(proxyurl + url);
-        const data = await responce.json();        
-            
-        return Data.extractData(data);
-    }
+  async getData() {
+    const { proxyurl, url } = this.state;
+
+    const responce = await fetch(proxyurl + url);
+    const data = await responce.json();
+
+    return Data.extractData(data);
+  }
 }
