d52fa698e2145b09c7b3e44b4d8ad9e216ce2d38
Sergey Hursevich <hursik94@gmail.com>
2019-05-18 07:45:38 +0300

    add: finally solver

--- src/js/App.js
+++ src/js/App.js
@@ -8,10 +8,14 @@ export default class App {
     };
   }
 
+  static isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
   async start() {
     const searchLength = document.querySelector('#btn-search-length');
     const searchSubstring = document.querySelector('#btn-search-substring');
-    // const checkbox = document.querySelector('#checkbox');
+    const checkbox = document.querySelector('#checkbox');
     const inputText = document.querySelector('#inputText');
     const textarea = document.querySelector('.textarea');
 
@@ -20,22 +24,52 @@ export default class App {
 
     searchLength.addEventListener('click', () => {
       const countWord = document.getElementsByTagName('p');
+      const { value } = inputText;
 
       if (countWord.length) {
         textarea.innerHTML = ' ';
       }
 
+      if (App.isNumeric(value)) {
+        arr.forEach((element) => {
+          if (value < element.length) {
+            textarea.innerHTML += `<p>${element}</p>`;
+          }
+        });
+      } else {
+        alert('Измените условия фильтра!');
+      }
+    });
+
+    searchSubstring.addEventListener('click', () => {
+      const countWord = document.getElementsByTagName('p');
       const { value } = inputText;
 
-      arr.forEach((element) => {
-        if (value.length < element.length) {
-          textarea.innerHTML += `<p>${element}</p>`;
+      if (countWord.length) {
+        textarea.innerHTML = ' ';
+      }
+
+      if (!App.isNumeric(value)) {
+        if (!checkbox.checked) {
+          arr.forEach((element) => {
+            if (element.toLowerCase().indexOf(value) !== -1) {
+              textarea.innerHTML += `<p>${element}</p>`;
+            }
+          });
+        } else {
+          arr.forEach((element) => {
+            if (element.indexOf(value) !== -1) {
+              textarea.innerHTML += `<p>${element}</p>`;
+            }
+          });
         }
-      });
+      } else {
+        alert('Измените условия фильтра!');
+      }
     });
 
     inputText.addEventListener('keyup', () => {
-      if (String(inputText.value).length > 1) {
+      if (String(inputText.value).length >= 1) {
         searchLength.classList.remove('btn__disable');
         searchLength.removeAttribute('disabled');
 
@@ -47,6 +81,8 @@ export default class App {
 
         searchLength.setAttribute('disabled', 'disabled');
         searchSubstring.setAttribute('disabled', 'disabled');
+
+        textarea.innerHTML = ' ';
       }
     });
   }
